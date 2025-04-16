46c230cec5823a9d71257654bcb062aeb7cade73
mahara0511 <122422522+mahara0511@users.noreply.github.com>
2025-04-16 23:03:13 +0700

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './cat/cat.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+console.log('DATABASE_HOST', process.env.DATABASE_HOST);
+console.log('DATABASE_PORT', process.env.DATABASE_PORT);
+console.log('DATABASE_USER', process.env.DATABASE_USER);
+console.log('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD);
+console.log('DATABASE_NAME', process.env.DATABASE_NAME);
 @Module({
     imports: [
         TypeOrmModule.forRoot({
